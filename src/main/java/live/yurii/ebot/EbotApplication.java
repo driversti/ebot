26dc0a55e9da5f5d84f73fb2f3d276e2256f177d
driversti <github@seniorjava.dev>
2025-11-08 11:51:13 +0100

    init commit

---
+++ src/main/java/live/yurii/ebot/EbotApplication.java
@@ -0,0 +1,13 @@
+package live.yurii.ebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EbotApplication.class, args);
+  }
+
+}
---
+++ src/test/java/live/yurii/ebot/EbotApplicationTests.java
@@ -0,0 +1,13 @@
+package live.yurii.ebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbotApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
